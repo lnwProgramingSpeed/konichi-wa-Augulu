d1cd306e4c56c9b946c812b9fef83973d698e801
Phawat Phuengdecha <pawat.pooh@gmail.com>
2023-07-30 15:28:18 +0700

    Work Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CaptionItem } from './caption-item';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,83 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'hello-angular';
+  title: string | null; // state variable
+  showImg: string | null;
+
+  captionsList: CaptionItem[] = [
+    {
+      id: 0,
+      message: 'Oh my',
+      icon: '../assets/WTF.jpg'
+    },
+    {
+      id: 1,
+      message: 'ARA ARA',
+      icon: `../assets/It's not your, it's ours.png`
+    },
+    {
+      id: 2,
+      message: 'NO HOMO',
+      icon: `../assets/gonna leave this here.jpg`
+    },
+    {
+      id: 3,
+      message: 'Holy shit',
+      icon: '../assets/magnificent women.jpg'
+    }
+  ];
+
+  currentData: CaptionItem[] = this.captionsList.slice();
+
+  constructor() {
+    const randomData = this.randomContient();
+    this.title = randomData.message;
+    this.showImg = randomData.icon;
+  }
+
+  handleRandom() {
+    if (this.currentData.length !== 0) {
+      const randomData = this.randomContient();
+      this.title = randomData.message;
+      this.showImg = randomData.icon;
+    }
+  }
+
+  randomContient() {
+    const randomIndex = this.random(this.currentData.length);
+    const randomData = this.currentData[randomIndex];
+
+    if (this.currentData.length !== 0) {
+      this.currentData.splice(randomIndex, 1); // remove that item from the list
+    }
+    
+    console.log(this.currentData);
+
+    return randomData;
+  } 
+
+  random(max: number) {
+    let currentElement = this.showImg;
+    let currentId = 0;
+    if (this.currentData.length <= 1) {
+      console.log('You have no images left to display');
+    } else {
+      while (currentElement === this.showImg) {
+        currentId = Math.floor(Math.random() * max);
+        currentElement = this.currentData[currentId].icon;
+      }
+    }
+    return currentId;
+  }
+
+  reset() {
+    this.currentData = [...this.captionsList];
+    const randomData = this.randomContient();
+    this.title = randomData.message;
+    this.showImg = randomData.icon;
+
+    if (this.currentData.length > 0) {
+      this.showImg = this.currentData[0].icon;
+    }
+  }
 }
